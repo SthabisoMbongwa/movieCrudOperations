27df7b28af01b5b938a52694ce7c776fac465a80
Sthabiso <sthabiso@aims.ac.za>
2024-01-24 16:02:40 +0200

    Created a users file and populated it with users

---
+++ movieCrud/user.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: "Sthabiso",
+        email: "sthabiso@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Space",
+        email: "smith@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Mashesha",
+        email: "Mashesha@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Sthembiso",
+        email: "Mbongwa@gmail.com"
+    },
+]
+
+module.exports = users;
